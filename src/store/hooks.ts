08b8e0fbed691c8e4f38768b5863c5e934c7777d
Rijan Shrestha <8daudionp20@gmail.com>
2026-02-23 10:50:11 +0545

    updated

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,6 @@
-import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
 import type { RootState, AppDispatch } from './index';
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch as unknown as () => AppDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
